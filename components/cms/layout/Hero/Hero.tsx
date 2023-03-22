de0e17971e7e4efcb7c5e4cc81e6150eae218b5d
fancn21th <fancn21th@gmail.com>
2023-03-22 10:32:57 +0800

    folder structure resemble the cms component structure

--- components/cms/layout/Hero/Hero.tsx
+++ components/cms/layout/Hero/Hero.tsx
@@ -1,20 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import { getStrapiMedia } from "utils";
 
 const HeroImage = styled.img`
   width: 100%;
   background: url(${(props) => props.src});
 `;
-
-export function getStrapiMedia(url) {
-  if (url == null) {
-    return null;
-  }
-  if (url.startsWith("http") || url.startsWith("//")) {
-    return url;
-  }
-  return `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337"}${url}`;
-}
 
 const Hero = ({ url }) => {
   return (
